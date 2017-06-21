b0a036cac0db8639e5f38677a1a0736ebd75403d
Matt <Matt>
2017-06-20 21:52:16 -0500

    Update to Show, CardList create
    Added comments and misc to Show
    Started CardList

--- src/CardList.java
+++ src/CardList.java
@@ -1,4 +1,73 @@
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
-public class CardList {
+/**
+ * This class creates a Show for a client holding a show name, clientID, and
+ * start and end date for the show. creation
+ * 
+ * @author Matt Carlson, Jamison Czech, Slava Makharovich, Prashant Shrestha
+ */
+
+public class CardList implements Serializable {
+
+	private static CardList cardList;
+	private List cards = new LinkedList();
+
+	/*
+	 * Private constructor to create singleton
+	 * 
+	 */
+	private CardList() {
+	}
+
+	/**
+	 * CardList singleton
+	 * 
+	 * @return the CardList singleton object
+	 */
+	public static CardList instance() {
+		if (cardList == null) {
+			return (cardList = new CardList());
+		} 
+		else {
+			return cardList;
+		}
+	}
+	
+	public boolean inserCard(CreditCard creditCard) {
+		cards.add(creditCard);
+		return true;
+	}
+	
+	public CreditCard search(String cardNumber) {
+	    for (Iterator iterator = cards.iterator(); iterator.hasNext(); ) {
+	      CreditCard creditCard = (CreditCard) iterator.next();
+	      if (creditCard.getCardNumber().equals(cardNumber)) {
+	        return creditCard;
+	      }
+	    }
+	    return null;
+	  }
+	
+	
+	public boolean removeCard(String cardNumber) {
+		CreditCard creditCard = search(cardNumber);
+		if (creditCard == null) {
+			return false;
+		}
+		else {
+			return cards.remove(creditCard);
+		}
+	}
+	
+	/**
+	 * String of the card
+	 */
+	@Override
+	public String toString() {
+		return cards.toString();
+	}
 
 }
