009c2d9d8ed7b9a91126d6a5bbe8543dbcf1339f
Matt <Matt>
2017-06-20 21:19:12 -0500

    Base Show class

--- src/Show.java
+++ src/Show.java
@@ -1,4 +1,38 @@
+import java.util.Calendar;
 
 public class Show {
+	
+	private String showName;
+	private String clientID;
+	private Calendar startDate;
+	private Calendar endDate;
+	
+	public Show(String showName, String clientID, Calendar startDate, Calendar endDate) {
+		this.showName = showName;
+		this.clientID = clientID;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
 
+	public String getShowName() {
+		return showName;
+	}
+
+	public String getClientID() {
+		return clientID;
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public Calendar getEndDate() {
+		return endDate;
+	}
+	
+	@Override
+	public String toString() {
+		return "Show: " + showName + " Client ID: " + clientID
+				+ " Start Date: " + startDate + " End Date " + endDate;
+	}
 }
