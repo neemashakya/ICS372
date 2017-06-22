7f302bb46038677a55e4f25e2712b0489f88fc26
Matt <MattDev31@users.noreply.github.com>
2017-06-21 20:26:56 -0500

    Small changes
    More interface changes and Theater singleton created to get rid of error

--- src/Theater.java
+++ src/Theater.java
@@ -1,4 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
 
 public class Theater {
+	
+	private static final long serialVersionUID = 1L;
+	private static Theater theater;
+	private List cards = new LinkedList();
 
+	/*
+	 * Private constructor to create singleton
+	 */
+	private Theater() {
+	}
+
+	/**
+	 * Theater singleton
+	 * @return the Theater singleton object
+	 */
+	public static Theater instance() {
+		if (theater == null) {
+			return (theater = new Theater());
+		} 
+		else {
+			return theater;
+		}
+	}
 }
